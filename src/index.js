0af723540a22017dc08ddfdeed6b6299d8d0f17f
Shannon Stephenson <stephenson.shannon29@gmail.com>
2022-05-09 18:08:56 -0400

    activity done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 import { store } from './store'
 import { Provider } from 'react-redux'
 
@@ -11,4 +11,4 @@ ReactDOM.render(
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
